3f37341f266e03f659ea59ed3a386e165850e2df
JordanNitta <Jordannittarodrigues@gmail.com>
2023-02-09 08:41:58 -0800

    Didn't solve last solution

--- Palindrome.js
+++ Palindrome.js
@@ -43,3 +43,43 @@ console.log(isPalindrome(str1))
 console.log(isPalindrome(str2))
 console.log(isPalindrome(str3))
 console.log(isPalindrome(str4))
+
+/* 
+  Longest Palindrome
+  For this challenge, we will look not only at the entire string provided,
+  but also at the substrings within it.
+  Return the longest palindromic substring. 
+  Strings longer or shorter than complete words are OK.
+  All the substrings of "abc" are:
+  a, ab, abc, b, bc, c
+*/
+
+const two_str1 = "what up, daddy-o?";
+const two_expected1 = "dad";
+
+const two_str2 = "uh, not much";
+const two_expected2 = "u";
+
+const two_str3 = "Yikes! my favorite racecar erupted!";
+const two_expected3 = "e racecar e";
+
+const two_str4 = "a1001x20002y5677765z";
+const two_expected4 = "5677765";
+
+const two_str5 = "a1001x20002y567765z";
+const two_expected5 = "567765";
+
+/**
+ * Finds the longest palindromic substring in the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The longest palindromic substring from the given string.
+ */
+function longestPalindromicSubstring(str) {
+    // Didn't solve
+}
+console.log(longestPalindromicSubstring(two_str1))
+console.log(longestPalindromicSubstring(two_str2))
+console.log(longestPalindromicSubstring(two_str3))
+console.log(longestPalindromicSubstring(two_str4))
