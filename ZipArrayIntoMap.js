f832f54754684c1f3ea59defacfd7fe0893a3873
JordanNitta <Jordannittarodrigues@gmail.com>
2023-02-10 09:06:05 -0800

    Solved

--- ZipArrayIntoMap.js
+++ ZipArrayIntoMap.js
@@ -1,3 +1,9 @@
+/* 
+Zip Arrays into Map
+
+Given two arrays, create an associative array (aka hash map, an obj / dictionary) containing keys from the first array, and values from the second.
+Associative arrays are sometimes called maps because a key (string) maps to a value 
+ */
 const keys1 = ["abc", 3, "yo",];
 const vals1 = [42, "wassup", true,];
 const expected1 = {
@@ -23,7 +29,9 @@ const expected2 = {};
 function zipArraysIntoMap(keys, values) {
     let obj = {};
     for (let i = 0; i < keys.length; i++) {
-        obj[keys[i]] = values[i]; 
+        let key = keys[i]
+        let value = values[i]
+        obj[key] = value;
     }
     return obj;
 }
@@ -31,3 +39,32 @@ function zipArraysIntoMap(keys, values) {
 console.log(zipArraysIntoMap(keys1, vals1))
 console.log(zipArraysIntoMap(keys2, vals2))
 
+
+/* 
+Invert Hash
+A hash table / hash map is an obj / dictionary
+Given an object / dict,
+return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
+*/
+
+const two_obj1 = { name: "Zaphod", charm: "high", morals: "dicey" };
+const two_expected1 = { Zaphod: "name", high: "charm", dicey: "morals" };
+
+// ****************** Potential Edge Cases *************************
+
+const two_obj2 = { name: "Zaphod", charm: "high", morals: "dicey", something: "dicey" };
+const two_expected2 = { Zaphod: "name", high: "charm", dicey: ["morals", "something"] };
+const two_expected3 = { Zaphod: "name", high: "charm", dicey: "morals", dicey2: "something" };
+
+/**
+ * Inverts the given object's key value pairs so that the original values
+ * become the keys and the original keys become the values.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object<string, any>} obj
+ * @return The given object with key value pairs inverted.
+ */
+// function invertObj(obj){
+
+// console.log(invertObj(two_obj1))
+// console.log(invertObj(two_obj2))
