2f565ee33e894e5de84626d4e96fee68b963a382
JordanNitta <Jordannittarodrigues@gmail.com>
2023-02-20 09:03:04 -0800

    First Commit

--- Social_Distance.js
+++ Social_Distance.js
@@ -1,10 +1,3 @@
-/* 
-Given an array of ints representing a line of people where the space between
-indexes is 1 foot, with 0 meaning no one is there and 1 meaning someone is in
-that space,
-return whether or not there is at least 6 feet separating every person.
-Bonus: O(n) linear time (avoid nested loops that cause re-visiting indexes).
-*/
 
 const queue1 = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1];
 const expected1 = false;
@@ -43,7 +36,8 @@ function socialDistancingEnforcer(queue) {
     return true
 }
 
-console.log(socialDistancingEnforcer(queue1)) // false
-console.log(socialDistancingEnforcer(queue2)) // true
-console.log(socialDistancingEnforcer(queue3)) // true
-console.log(socialDistancingEnforcer(queue4)) // true
+console.log(socialDistancingEnforcer(queue1))
+console.log(socialDistancingEnforcer(queue2))
+console.log(socialDistancingEnforcer(queue3))
+console.log(socialDistancingEnforcer(queue4))
+
